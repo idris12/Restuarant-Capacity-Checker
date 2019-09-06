c8be39393c09eb7c725210965515281f64cf0ef0
idris <37313213+idris12@users.noreply.github.com>
2019-09-06 15:05:52 -0400

    Update object-methods.js

--- object-methods.js
+++ object-methods.js
@@ -3,34 +3,38 @@ let restuarant = {
     guestCapacity: 75,
     guestCount: 0,
     
-    
+    // Assigns number of people seated to guestCount
     seatedParty: function(numOfparty){
         this.guestCount = numOfparty
     },
-
+    // Substracts number of people leaving from guest count
     removedParty: function(numOfPartyRemoved){
         this.guestCount = this.guestCount - numOfPartyRemoved
     },
 
-    
+    // Checks if people who want to seat are greater than guest count and returns a boolean
     checkAvailability: function (partySize) {
         let seatsLeft = this.guestCapacity - this.guestCount
         return partySize <= seatsLeft
     }
 }
 
-// seatParty
-// RemovedParty
 
+// Adds 72 to guest count
 restuarant.seatedParty(72)
+
+// Checks to see if there avaliability
 console.log( restuarant.checkAvailability(4))
-console.log(restuarant)
+
+// subtracts 5 from guest count
 restuarant.removedParty(5)
+
+// Checks to see if there avaliability
 console.log( restuarant.checkAvailability(4))
-console.log(restuarant)
 
 
 
 
-// console.log( restuarant.checkAvailability(60))
+
+
  
